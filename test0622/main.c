30dd0e5afc7a42b2d5fdf69901c94391c8bf18d4
20161104569 <2174037646@qq.com>
2017-06-22 09:42:15 +0800

    Initial Commit

---
+++ test0622/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test0622
+//
+//  Created by 20161104569 on 17/6/22.
+//  Copyright © 2017年 20161104569. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
