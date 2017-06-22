c906126dc76a92a462c1109f6f62b88ab998e759
20161104569 <2174037646@qq.com>
2017-06-22 10:00:06 +0800

    GPRS
    GPRS

--- test0622/main.c
+++ test0622/main.c
@@ -10,6 +10,19 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    FILE  *fp;
+    //FILE  *fw;
+    char temp1[80];
+    char temp2[80];
+    char lat[10];
+    int i;
+    fp=fopen("//Users//a20161104569//Desktop//test0622 //GPS170510.log","r+");
+    fscanf(fp,"%s%s",temp1,temp2);
+    printf("结果:%s\n%s\n",temp1,temp2);
+    for(i=0;i<8;i++)
+        lat[i]=temp1[i+16];
+    lat[8]='\0';
+    printf("纬度:%s\n",lat);
+    fclose(fp);
     return 0;
 }
